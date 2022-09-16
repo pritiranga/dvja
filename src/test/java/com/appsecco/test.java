a4dd76b25800d460b1e93cb8677a7a8b443c95d2
Priti <61485216+pritiranga@users.noreply.github.com>
2022-09-16 12:11:07 +0530

    Update test.java

--- src/test/java/com/appsecco/test.java
+++ src/test/java/com/appsecco/test.java
@@ -1,16 +1,19 @@
-package com.appsecco.example;
-import org.junit.Test;
-import static org.junit.Assert.*;
-
-public class MyUnitTest {
-
-    @Test
-    public void testConcatenate() {
-        MyUnit myUnit = new MyUnit();
-
-        String result = myUnit.concatenate("one", "two");
-
-        assertEquals("onetwo", result);
-
+package com.crunchify.junit;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+/**
+ * @author Crunchify
+ * 
+ */
+public class CrunchifyRunnerJUnit {
+    public static void main(String[] args) {
+        Result result = JUnitCore.runClasses(CrunchifyJunitTest.class);
+        for (Failure failure : result.getFailures()) {
+            System.out.println(failure.toString());
+        }
+        if (result.wasSuccessful()) {
+            System.out.println("Both Tests finished successfully...");
+        }
     }
 }
